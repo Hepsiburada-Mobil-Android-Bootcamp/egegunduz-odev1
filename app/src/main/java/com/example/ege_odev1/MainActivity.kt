e67e1545cc6f1d6e74985b23ce590d7254333f7d
Ege <m.egegunduz@gmail.com>
2021-10-11 15:54:31 +0300

    initial commit

---
+++ app/src/main/java/com/example/ege_odev1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ege_odev1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
