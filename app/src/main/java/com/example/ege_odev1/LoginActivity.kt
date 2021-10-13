65e79b3ebfc5c4c10e80b659b56efe8fd8b99038
Ege Gündüz <m.egegunduz@gmail.com>
2021-10-14 02:04:46 +0300

    clicking login button starts HomeActivity

--- app/src/main/java/com/example/ege_odev1/LoginActivity.kt
+++ app/src/main/java/com/example/ege_odev1/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.ege_odev1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    val loginButton by lazy { findViewById<View>(R.id.button_login) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
