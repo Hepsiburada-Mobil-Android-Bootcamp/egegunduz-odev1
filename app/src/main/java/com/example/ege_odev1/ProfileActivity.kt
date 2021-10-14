82f6e4c7ae38058b9dc00c2e594e0ab5a79363f3
Ege Gündüz <m.egegunduz@gmail.com>
2021-10-15 00:40:47 +0300

    home changed to profile

--- app/src/main/java/com/example/ege_odev1/ProfileActivity.kt
+++ app/src/main/java/com/example/ege_odev1/ProfileActivity.kt
@@ -2,11 +2,10 @@ package com.example.ege_odev1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_profile)
     }
 }
