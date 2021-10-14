93456aa0ee48dd6ee78f7d1373abc04270601f61
Ege Gündüz <m.egegunduz@gmail.com>
2021-10-14 18:49:48 +0300

    second screen completed

--- app/src/main/java/com/example/ege_odev1/HomeActivity.kt
+++ app/src/main/java/com/example/ege_odev1/HomeActivity.kt
@@ -5,13 +5,8 @@ import android.os.Bundle
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
-
-    val homeTextView by lazy { findViewById<TextView>(R.id.textview_home)}
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        homeTextView.text = "Home Activity"
     }
 }
